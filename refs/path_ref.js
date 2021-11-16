0d1b0367ea18b3e5b9762af03cfd8c72406c6c10
Maxim-Rider <maksitrall5@gmail.com>
2021-11-16 10:12:57 +0200

    Part 2: Path (Updated)

--- refs/path_ref.js
+++ refs/path_ref.js
@@ -1,11 +1,11 @@
-const path = require('path')
+const path = require('path');               // Подгружаем модуль path
 
-console.log(__filename)
-console.log(path.basename(__filename))
-console.log(path.dirname(__filename))
-console.log(path.extname(__filename))
+console.log(__filename);                    // Возвращает полный путь к файлу
+console.log(path.basename(__filename));     // Возвращает имя файла с форматом
+console.log(path.dirname(__filename));      // Возвращает полный путь к папке с файлом
+console.log(path.extname(__filename));      // Возвращает формат файла
 
-console.log(path.parse(__filename))
+console.log(path.parse(__filename));        // Возвращает объект, содержащий информацию о пути файла
 
-console.log(path.join(__dirname, 'test', 'second.html'))
-console.log(path.resolve(__dirname, '/test', '/second.html'))
+console.log(path.join(__dirname, 'test', 'second.html'));           // Возвращает путь, образованный простым склеиванием папки файла и данных аргументов
+console.log(path.resolve(__dirname, './test', '/second.html'));     // Возвращает путь, образованный непростым склеиванием папки файла и данных переменных 
