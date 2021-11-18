301597b7a93289b6d6c3930b5812d7214cae8e62
Maxim-Rider <maksitrall5@gmail.com>
2021-11-18 08:31:40 +0200

    Part 7: HTTP

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-const obj = require('./user')
+// const obj = require('./user')
 
-console.log(obj.user)
+// console.log(obj.user)
 
-obj.sayHello()
+// obj.sayHello()
 
-console.log("work dir", __dirname)
-console.log("work file", __filename)
+// console.log("work dir", __dirname)
+// console.log("work file", __filename)
+
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    console.log(req.url)
+
+    res.write('<h1>Hello from NodeJS</h1>')
+    res.write('<h2>Hello from NodeJS</h2>')
+    res.write('<h3>Hello from NodeJS</h3>')
+    res.end(
+        `
+        <div style="background: red; width: 200px; height: 200px;">
+            <h1>Test</h1>
+        </div>
+        `
+    )
+})
+
+server.listen(3000, () => {
+    console.log('Server is running...')
+})
